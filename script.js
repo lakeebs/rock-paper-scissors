c0fc93914ff53468db31313c1a5f7c6856292cb9
John Lee <info@keebs.com>
2024-02-18 23:12:56 +0900

    Add default message for wrong user input

--- script.js
+++ script.js
@@ -1,50 +1,55 @@
-// Randomly returns either rock, paper, or scissors
+// Randomly return either rock, paper, or scissors
 function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     const choice = choices[Math.floor(Math.random() * choices.length)];
     return choice;
 }
 
-// Plays a single round
+// Play a single round
 function playRound(playerSelection, computerSelection) {
 
     switch(computerSelection) {
         case "Paper":
-            if (playerSelection === "rock") {
-                return "The computer chose paper. You lose! Paper beats rock.";
+            switch (playerSelection) {
+                case "rock":
+                    return "The computer chose paper. You lose! Paper beats rock.";
+                case "scissors":
+                case "scissor":
+                    return "The computer chose paper. You win! Scissors beats paper.";
+                case "paper":
+                    return "You and the computer both chose paper. Tie!";
+                default:
+                    return "Did you mistype something?";
             }
-            if (playerSelection === "scissors" || playerSelection === "scissor") {
-                return "The computer chose paper. You win! Scissors beats paper.";
-            }
-            if (playerSelection === "paper") {
-                return "Tie!";
-            }
-            break;
+
         case "Rock":
-            if (playerSelection === "paper") {
-                return "The computer chose rock. You win! Paper beats rock.";
+            switch (playerSelection) {
+                case "paper":
+                    return "The computer chose rock. You win! Paper beats rock.";
+                case "scissors":
+                case "scissor":
+                    return "The computer chose rock. You lose! Rock beats scissors.";
+                case "rock":
+                    return "You and the computer both chose rock. Tie!";
+                default:
+                    return "Did you mistype something?";
             }
-            if (playerSelection === "scissors" || playerSelection === "scissor") {
-                return "The computer chose rock. You lose! Rock beats scissors.";
-            }
-            if (playerSelection === "rock") {
-                return "Tie!";
-            }
-            break;
+
         case "Scissors":
-            if (playerSelection === "rock") {
-                return "The computer chose scissors. You win! Rock beats scissors.";
+            switch (playerSelection) {
+                case "rock":
+                    return "The computer chose rock. You win! Paper beats rock.";
+                case "paper":
+                    return "The computer chose scissors. You lose! Scissors beats paper.";
+                case "scissors":
+                case "scissor":
+                    return "You and the computer both chose scissors. Tie!";
+                default:
+                    return "Did you mistype something?";
             }
-            if (playerSelection === "paper") {
-                return "The computer chose scissors. You lose! Scissors beats paper.";
-            }
-            if (playerSelection === "scissors" || playerSelection === "scissor") {
-                return "Tie!";
-            }
-            break;
             
         default:
-            return "Did you mistype something?";
+            return "Invalid selection by the computer.";
     }
 
 }
@@ -53,7 +58,7 @@ const playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
 
-// Plays a five round game
+// Play a five round game
 function playGame() {
     
 }
